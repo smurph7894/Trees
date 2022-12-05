0ffd0e8a5adac4a877307532ecf249eeec7b6931
smurph7894 <104289709+smurph7894@users.noreply.github.com>
2022-12-04 22:32:09 -0800

    finished basic

--- PracticeOne.js
+++ PracticeOne.js
@@ -17,7 +17,7 @@ class BST {
     //Add
     add(val){
         if (this.root === null){
-            this.root = val;
+            this.root = new BTNode(val);
         }
         else {
             this.insert(val, this.root);
@@ -85,10 +85,145 @@ class BST {
         }
     }
 
+    max(){
+        return this.largestValue(this.root);
+    }
+    largestValue(node){
+        if(node === null){
+            return node.val;
+        }
+        else if ( node.right === null ){
+            return node.val;
+        }
+        else {
+            return this.largestValue(node.right);
+        }
+    }
+
+    size(){
+        return this.countNodes(this.root);
+    }
+    countNodes(node){
+        if (node === null){
+            return 0;
+        }
+        else if ( node.right === null && node.left === null ){
+            return 1;
+        }
+        else {
+            const right = this.countNodes(node.right);
+            const left = this.countNodes(node.left);
+            return left + right + 1;
+        }
+    }
+
+    isEmpty(){
+        if (this.root === null){
+            return true;
+        }
+        else {
+            return false;
+        }
+        // or return this.root === null (returns true or false)
+    }
+
+    sumAll(){
+        return this.sumNodes(this.root);
+    }
+    sumNodes(node){
+        if (node === null){
+            return 0;
+        }
+        else if ( node.right === null && node.left === null ){
+            return node.val;
+        }
+        else {
+            const right = this.sumNodes(node.right);
+            const left = this.sumNodes(node.left);
+            return left + right + node.val;
+        }
+    }
+
+    printInOrder(){
+        this.inOrder(this.root);
+    }
+    inOrder(node){
+        if( node === null){
+            return;
+        }
+        if (node.left != null){
+            this.inOrder(node.left);
+        }
+        console.log(node.val);
+        if (node.right != null){
+            this.inOrder(node.right);
+        }
+    }
+
+    printPreOrder(){
+        this.preOrder(this.root);
+    }
+    preOrder(node){
+        if( node === null){
+            return;
+        }
+        console.log(node.val);
+        if (node.left != null){
+            this.preOrder(node.left);
+        }
+        if (node.right != null){
+            this.preOrder(node.right);
+        }
+    }
+
+    printPostOrder(){
+        this.postOrder(this.root);
+    }
+    postOrder(node){
+        if( node === null){
+            return;
+        }
+        if (node.left != null){
+            this.postOrder(node.left);
+        }
+        if (node.right != null){
+            this.postOrder(node.right);
+        }
+        console.log(node.val);
+    }
+
 }
 
+// BST.prototype.toString= ()=>{return "hello";};
 
+treeOne = new BST();
 
+treeOne.add(5);
+treeOne.add(1);
+treeOne.add(2);
+treeOne.add(3);
+treeOne.add(4);
+treeOne.add(9);
+treeOne.add(11);
+treeOne.add(13);
+treeOne.add(15);
+
+// console.log(treeOne.contains(9));
+// console.log(treeOne.contains(20));
+
+// console.log("min", treeOne.min());
+// console.log("max", treeOne.max());
+// console.log("size", treeOne.size());
+
+// // console.log("inOrder");
+// // treeOne.printInOrder();
+
+// console.log("PostOrder");
+// treeOne.printPostOrder();
+
+// console.log("PreOrder");
+// treeOne.printPreOrder();
+console.log("sum", treeOne.sumAll());
 
 // BST: Add
 // Create an add(val) method on the BST object to add new value to the tree. This entails creating a BTNode with this value and connecting it at the appropriate place in the tree. Unless specified otherwise, BSTs can contain duplicate values.
